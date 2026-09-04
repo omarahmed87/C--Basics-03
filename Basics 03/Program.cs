da8a5d12655a89681650ce6a4220425935d0aa5e
Omar Ahmed <cresssoomar@gmail.com>
2026-09-04 18:24:52 +0300

    14st answer

--- Basics 03/Program.cs
+++ Basics 03/Program.cs
@@ -1,26 +1,15 @@
-﻿#region 13st answer
-//Write code that divides 10 by 0 inside a try block, catches the exception, prints "Cannot 
-//divide by zero", and then prints "Done" in a finally block. 
+﻿
+#region 14st answer
+//Declare an int pages = 300; then store it in a double variable without using a cast.
+
 #endregion
-using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        try
-        {
-            int num1 = 10;
-            int num2 = 0;
-            int result = num1 / num2;
-        }
-        catch (DivideByZeroException)
-        {
-            Console.WriteLine("Cannot divide by zero");
-        }
-        finally
-        {
-            Console.WriteLine("Done");
-        }
+        int pages = 300;
+        double newPages = pages;
     }
 }
+  
