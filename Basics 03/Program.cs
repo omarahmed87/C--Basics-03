938f6ea95afa56dc2b54c3b2ae4bfdfeed01ce4d
Omar Ahmed <cresssoomar@gmail.com>
2026-09-04 16:23:49 +0300

    1st answer

--- Basics 03/Program.cs
+++ Basics 03/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿#region 1st answer
+//Given int pages = 464;, write an if / else statement that prints "Long Book"
+//if pages is greater than 300, otherwise prints "Short Book".   
+#endregion
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = 464;
+            if (pages >= 300)
+            {
+                Console.WriteLine("Long Book");
+            }
+            else
+            {
+                Console.WriteLine("Short Book");
+            }
+        }
+    }
+}
